8620a86621076d0a784ad8fde4e27badc6a8763d
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2025-06-27 19:58:58 +0300

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Dmitry on 27.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
